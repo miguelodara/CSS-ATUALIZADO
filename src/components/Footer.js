c88f9569d5b241d7f3f291049e34bbdbb34b1c6c
miguelodara <113523094+miguelodara@users.noreply.github.com>
2023-03-17 10:18:06 -0300

    Add files via upload

---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import styles from "./Footer.module.css";
+
+const Footer = () => {
+  return (
+    <footer className={styles.footer}>
+      <h3>O SITE PREFERIDO DE TODO INTERNAUTA!</h3>
+      <p>Edit-On &copy; 2023</p>
+    </footer>
+  );
+};
+
+export default Footer;
